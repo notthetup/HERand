f2cbcf1f9b51d03fbe01b852d35efdb620c8cb7d
Chinmay Pendharkar <notthetup@gmail.com>
2013-02-18 12:20:31 +0800

    renamed file

---
+++ HERand.sh
@@ -0,0 +1,1 @@
+curl -s https://www.grc.com/OffTheGrid.htm | grep serverEntropy | cut -f 2 -d '"'
---
+++ HRRand.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -s https://www.grc.com/OffTheGrid.htm | grep serverEntropy | cut -f 2 -d '"'
